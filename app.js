b0f02609c912c0647b9fe5eeefae00f0c69d538d
suportetiandarai <suporteandaraiti@gmail.com>
2026-05-04 11:02:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // ==========================================
 // 1. CONFIGURAÇÃO DO SUPABASE (COLE SUAS CHAVES AQUI)
 // ==========================================
-const SUPABASE_URL = 'COLE_AQUI_SUA_URL';
-const SUPABASE_ANON_KEY = 'COLE_AQUI_SUA_CHAVE_ANON';
+const SUPABASE_URL = 'https://ygnphizpnhcsblmwzmzj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlnbnBoaXpwbmhjc2JsbXd6bXpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MzUyNjAsImV4cCI6MjA5MjAxMTI2MH0.hLhpjB5WUDzZX1MRIPVzPVFgq8mcHmnhkhWreAjEFXI';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // ==========================================
